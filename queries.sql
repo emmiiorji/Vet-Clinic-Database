6259bec38320b37dfb16da52ea74785e1d085806
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-18 13:41:48 +0100

    Add query to select name and escape_attempts of animals weighing more than 10.5

--- queries.sql
+++ queries.sql
@@ -7,3 +7,5 @@ SELECT * from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT * from animals WHERE neutered = TRUE AND escape_attempts < 3;
 
 SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu');
+
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
