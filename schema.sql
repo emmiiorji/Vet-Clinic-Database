ef22afb0102298f26245ac43d7c983500657438f
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-18 12:50:05 +0100

    Modify constraint for escape_attempts column

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name varchar(100) NOT NULL,
     date_of_birth DATE NOT NULL,
-    escape_attempts INT DEFAULT 0 CHECK(escape_attempts > 0),
+    escape_attempts INT DEFAULT 0 CHECK(escape_attempts >= 0),
     neutered BOOLEAN DEFAULT FALSE,
     weight_kg FLOAT NOT NULL CHECK(weight_kg > 0.0)
 );
