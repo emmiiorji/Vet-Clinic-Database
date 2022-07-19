45b4119ee80f8d636cf0df18637799f69ba0a0b4
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-19 03:49:45 +0100

    Add command to delete animals table and roll back changes

--- queries.sql
+++ queries.sql
@@ -27,3 +27,10 @@ BEGIN;
   UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
   UPDATE animals SET species='pokemon' WHERE species IS NULL;
 COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+  DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
