912c596f13a42a4b3953f6d80eda6ecc7432feb6
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-20 04:35:20 +0100

    Update species_id column with IDs from species table as foreign keys

--- data.sql
+++ data.sql
@@ -31,10 +31,17 @@ INSERT INTO
     ('Melody Pond', 77),
     ('Dean Winchester', 14),
     ('Jodie Whittaker', 38)
-RETURNING *
+RETURNING *;
 
 INSERT INTO
   species(name)
   VALUES
     ('Pokemon'),
     ('Digimon')
+RETURNING *;
+
+UPDATE animals
+  SET species_id=2 WHERE name LIKE '%mon';
+
+UPDATE animals
+  SET species_id=1 WHERE name NOT LIKE '%mon';
