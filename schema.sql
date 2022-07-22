6f8ae2e3806917236b1486b78dc2a631543ed27b
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-22 18:42:17 +0100

    Add primary key constraint to visits table

--- schema.sql
+++ schema.sql
@@ -55,7 +55,8 @@ CREATE TABLE specializations(
 );
 
 CREATE TABLE visits(
-    animal_id INT REFERENCES animals(id) NOT NULL,
-    vet_id INT REFERENCES vets(id) NOT NULL,
-    visit_date DATE NOT NULL
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    visit_date DATE NOT NULL,
+    PRIMARY KEY(animal_id, vet_id)
 );
