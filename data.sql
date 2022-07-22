5627e9207fdfff7249ad256b6fbcc9ecfe6cc2e1
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-22 18:23:33 +0100

    Include missing VALUES keyword in statement to populate vets table

--- data.sql
+++ data.sql
@@ -69,6 +69,7 @@ COMMIT;
 
 INSERT INTO
   vets(name, age, date_of_graduation)
+  VALUES
   ('William Tatcher', 45, '2000-04-23'),
   ('Maisy Smith', 26, '2019-01-17'),
   ('Stephanie Mendez', 64, '1981-05-04'),
