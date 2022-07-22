9d49e931804c022ab9604f549d4ce886ce89060e
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-22 18:20:13 +0100

    Add data to vets table

--- data.sql
+++ data.sql
@@ -66,3 +66,11 @@ BEGIN;
     SET owner_id=5 WHERE name='Angemon' OR name='Boarmon';
 
 COMMIT;
+
+INSERT INTO
+  vets(name, age, date_of_graduation)
+  ('William Tatcher', 45, '2000-04-23'),
+  ('Maisy Smith', 26, '2019-01-17'),
+  ('Stephanie Mendez', 64, '1981-05-04'),
+  ('Jack Harkness', 38, '2008-06-08')
+RETURNING *;
