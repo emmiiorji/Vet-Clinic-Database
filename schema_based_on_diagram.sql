e9fa7ff58733c0f8208c534ca32ec9bb9a487d50
emmiiorji <emmanuelorji2013@gmail.com>
2022-07-27 07:29:18 +0100

    Create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,10 @@ CREATE TABLE patients (
   name VARCHAR(100) NOT NULL,
   date_of_birth DATE NOT NULL
 )
+
+CREATE TABLE medical_histories (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  admitted_at TIMESTAMP NOT NULL,
+  patient_id INT REFERENCES patients(id),
+  status VARCHAR(100) NOT NULL
+)
